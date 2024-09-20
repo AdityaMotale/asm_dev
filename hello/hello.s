6193cc2c97934fab9f65a3fecaff48e5c69a17ef
Adii <77727680+AdityaMotale@users.noreply.github.com>
2024-09-20 21:13:36 +0530

    hello (hello world in x86-32 bit, WSL 2 Ubuntu)

---
+++ hello/hello.s
@@ -0,0 +1,26 @@
+# x86 Hello World (32 bit)
+#
+# Compile with:
+# as -32 hello.s -o hello.o
+# ld -m elf_i386 -o hello hello.o -no-pie
+
+.global _start
+.section .text
+
+_start:
+
+    # Write call
+    mov $4, %eax # set the sys call type
+    mov $1, %ebx # STDOUT code
+    lea message, %ecx
+    mov $13, %edx
+    int $0x80
+
+    # Exit call
+    mov $1, %eax
+    mov $0, %ebx
+    int $0x80
+
+.section .data
+    message:
+    .ascii "Hello, World\n"
