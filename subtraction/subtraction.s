6996a1c0db351121e6cc92ff120268b8d3e85968
Adii <77727680+AdityaMotale@users.noreply.github.com>
2024-09-21 19:45:03 +0530

    subtraction (subtracting two numbers and printing the output w/ x86-32 bit, WSL 2 Ubuntu)

--- subtraction/subtraction.s
+++ subtraction/subtraction.s
@@ -1,13 +1,13 @@
-# x86 Addition (32 bit)
+# x86 Subtraction (32 bit)
 #
 # Compile with:
-# as -32 addition.s -o addition.o
-# ld -m elf_i386 -o addition addition.o -no-pie
+# as -32 subtraction.s -o subtraction.o
+# ld -m elf_i386 -o subtraction subtraction.o -no-pie
 
 .global _start
 
 .section .data
-    msg: .ascii "After adding 60 + 30, we get: "
+    msg: .ascii "After subtracting 10 from 90, we get: "
     msg_len = . - msg
     
     new_line: .ascii "\n"
@@ -18,29 +18,28 @@
 .section .text
 
 _start:
-
-    # Print the starting text
+    # Print starting text
     mov $4, %eax
     mov $1, %ebx
     lea msg, %ecx
     mov $msg_len, %edx
     int $0x80
 
-    # Perform Addition
-    mov $60, %eax
-    mov $30, %ebx
+    # Perform subtraction
+    mov $90, %eax
+    mov $10, %ebx
+    sub %ebx, %eax
 
-    add %ebx, %eax
     mov %eax, result
 
-    # Print addition result
+    # Print result
     mov $4, %eax
     mov $1, %ebx
     lea result, %ecx
-    mov $10, %edx
+    mov $4, %edx
     int $0x80
 
-    # Print the new_line character
+    # Print new_line char
     mov $4, %eax
     mov $1, %ebx
     lea new_line, %ecx
